cfe48c988a8aaad3e41646e959ae47ddadc0c055
Candace Schenk <candace.schenk@gmail.com>
2014-03-09 13:24:10 -0400

    add guess_a_letter to human and computer player

--- hangman.rb
+++ hangman.rb
@@ -1,20 +1,39 @@
+
+ALPHABET = ('a'..'z').to_a
+
 class Hangman
   attr_accessor :guessed_letters, :revealed_word
 
-  def initialize(setter = ComputerPlayerDumb.new, guesser = HumanPlayer.new, revealed_word)
+  def initialize(setter = ComputerPlayerDumb.new, guesser = HumanPlayer.new)
     @setter = setter
     @guesser = guesser
     @revealed_word = setter.revealed_word # maybe make this get the reavled word from the player
+    @guessed_letters = []
   end
 
 end
 
 class HumanPlayer
   attr_reader :revealed_word
-
+  attr_accessor :guessed_letters
+  
+  def initialize
+    @guessed_letters = []
+  end
+  
+  def guess_a_letter # need an until, as so the human cannot repeat guesses, and also, inputs a letter
+    puts "Guess a letter"
+    guess = gets.chomp
+    until (ALPHABET- guessed_letters).include?(guess)
+      puts "Try again"
+      guess = gets.chomp
+    end
+    guessed_letters << guess
+    guess
+  end
 
   # private ?
-  def get_length
+  def get_length # need to check input (make sure it is a number)
    puts "How long is your word?"
    word_length = gets.chomp.to_i
    @revealed_word = Array.new(word_length) 
@@ -23,12 +42,22 @@ class HumanPlayer
 end
 
 class ComputerPlayerDumb
-  attr_reader :revealed_word
   
-    def initialize
-        @secret_word = secret_word
-        @revealed_word = hide_word
-      end
+  attr_reader :revealed_word
+  attr_accessor :guessed_letters
+
+  
+  def initialize
+    @secret_word = secret_word
+    @revealed_word = hide_word
+    @guessed_letters = []
+  end
+  
+  def guess_a_letter
+    pos_letters = ALPHABET - guessed_letters 
+    guess = pos_letters.sample
+    guessed_letters << guess
+  end
        
    # private
   
