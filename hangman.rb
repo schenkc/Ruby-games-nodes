73e6cc5b0ccb119eb2e99e1ed8350d02489e653f
Candace Schenk <candace.schenk@gmail.com>
2014-03-09 21:04:31 -0400

    Debugging HumanPlayer as setter-- not working still

--- hangman.rb
+++ hangman.rb
@@ -1,14 +1,54 @@
 
+require 'debugger'
+
 ALPHABET = ('a'..'z').to_a
+MAX_NUM_BAD_GUESSES = 10
 
 class Hangman
-  attr_accessor :guessed_letters, :revealed_word
+  attr_accessor :revealed_word
 
   def initialize(setter = ComputerPlayerDumb.new, guesser = HumanPlayer.new)
     @setter = setter
     @guesser = guesser
-    @revealed_word = setter.revealed_word # maybe make this get the reavled word from the player
+    @revealed_word = @setter.set_revealed_word # maybe make this get the reavled word from the player
     @guessed_letters = []
+  end
+  
+  def run
+   # debugger
+   # @setter.set_revealed_word
+    until won? || lost?
+      puts "Maybe I should have the number of guesses left here."
+      print "You've guessed: #{@guesser.guessed_letters.join(', ')}\n"
+      render_revealed_word
+      guess = @guesser.guess_a_letter
+      @setter.update_revealed_word(guess)
+    end
+    if won?
+      puts "You guessed my word!"
+    elsif lost?
+      puts "Better luck next time."
+    end
+  end
+  
+  def render_revealed_word
+    result = []
+    revealed_word.each do |letter|
+      if letter =~ /\w/
+        result << letter
+      else
+        result << "_"
+      end
+    end
+    puts "Secret word: #{result.join(' ')}"
+  end
+  
+  def won?
+    revealed_word.length == revealed_word.compact.length
+  end
+  
+  def lost?
+    (@guesser.guessed_letters - revealed_word.compact).length == MAX_NUM_BAD_GUESSES
   end
 
 end
@@ -19,13 +59,16 @@ class HumanPlayer
   
   def initialize
     @guessed_letters = []
+    @revealed_word = []
   end
   
-  def guess_a_letter # need an until, as so the human cannot repeat guesses, and also, inputs a letter
+  def guess_a_letter 
     puts "Guess a letter"
+    print "> "
     guess = gets.chomp
     until (ALPHABET- guessed_letters).include?(guess)
       puts "Try again"
+      print "> "
       guess = gets.chomp
     end
     guessed_letters << guess
@@ -33,29 +76,32 @@ class HumanPlayer
   end
 
   # private ?
-  def get_length # need to check input (make sure it is a number)
+  def set_revealed_word # need to check input (make sure it is a number)
    puts "How long is your word?"
+   print "> "
    word_length = gets.chomp.to_i
    @revealed_word = Array.new(word_length) 
   end
   
   def update_revealed_word(str)
-    number_letter_in_word(str).to_i.times do |i|
-      puts render_revealed_word
-      numbers = []
-      (1..revealed_word.length).each do |i|
-        numbers << i
-      end
-      puts numbers.join("\t")
-      begin
-        puts "Where does #{str} occur in your word?"
-        print "> "
-        index = gets.chomp
-        Integer(index)
-      rescue
-        retry
-      end
+    if letter_in_word(str)
+      number_letter_in_word(str).to_i.times do |i|
+        puts render_revealed_word
+        numbers = []
+        (1..revealed_word.length).each do |i|
+          numbers << i
+        end
+        puts numbers.join("\t")
+        begin
+          puts "Where does #{str} occur in your word?"
+          print "> "
+          index = gets.chomp
+          Integer(index)
+        rescue
+          retry
+        end
       @revealed_word[index.to_i - 1] = str
+      end
     end
     
   end
@@ -106,7 +152,7 @@ class ComputerPlayerDumb
   
   def initialize
     @secret_word = secret_word
-    @revealed_word = hide_word
+    @revealed_word = set_revealed_word
     @guessed_letters = []
   end
   
@@ -132,6 +178,14 @@ class ComputerPlayerDumb
   def letter_in_word?(str)
     @secret_word.include?(str)
   end
+  
+  def set_revealed_word
+    result = []
+    @secret_word.each_char do |letter|
+      letter =~ /[a-zA-Z]/ ? result << nil : result << letter
+    end
+    result
+  end
        
    # private
   
@@ -140,13 +194,7 @@ class ComputerPlayerDumb
     @secret_word = words.sample.strip
   end
   
-  def hide_word
-    result = []
-    @secret_word.each_char do |letter|
-      letter =~ /[a-zA-Z]/ ? result << nil : result << letter
-    end
-    result
-  end
+
 
 end
 
