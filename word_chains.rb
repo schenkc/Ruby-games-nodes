16181c084b8887f9ce119b3c8c6d8958cdc55d95
Candace Schenk <candace.schenk@gmail.com>
2014-03-14 07:50:19 -0400

    no idea

--- word_chains.rb
+++ word_chains.rb
@@ -15,13 +15,26 @@ attr_reader :dictionary
   end
   
   def one_letter_different?(test_word, word)
+    return false if test_word.length != word.length #maybe not needed
     num_diff = 0
     (0...word.length).each { |i| num_diff += 1 if word[i] != test_word[i] }
     num_diff == 1 ? true : false
   end
   
   def word_list_correct_length(num)
-    possible_word << dictionary.selection { |test_word| test_word.length == num }
+    possible_words + dictionary.select { |test_word| test_word.length == num }
+  end
+  
+  def build_tree(start_word, end_word)
+    raise "Can't connect #{start_word} to #{end_word} as they aren't the same length" if start_word.length != end_word.length
+    word_list_correct_length(start_word.length)
+    # building the tree should loop till adjacent_words.empty?
+    tree = TreeNode.new(start_word)
+    possible_words -= [start_word]
+    children = adjacent_words(start_word, possible_words)
+    children.each do |child|
+      TreeNode.new(child)
+    end
   end
   
 end
